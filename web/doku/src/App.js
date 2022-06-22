2ded6592454939156f4cc795d06f00acb55ca44c
amerkurev <apps.merkurev@gmail.com>
2022-06-23 01:14:43 +0300

    configure reactapp project (deps, linter, prettier...)

--- web/doku/src/App.js
+++ web/doku/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
