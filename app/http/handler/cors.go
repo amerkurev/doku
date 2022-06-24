c976123f60cd46e73f6df8907d833eb52467d1e2
amerkurev <apps.merkurev@gmail.com>
2022-06-24 22:25:27 +0300

    fix linter warn

--- app/http/handler/cors.go
+++ app/http/handler/cors.go
@@ -2,6 +2,8 @@ package handler
 
 import "net/http"
 
+// DevCORS allows restricted resources on a web page to be requested from another domain outside
+// the domain from which the first resource was served.
 func DevCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
